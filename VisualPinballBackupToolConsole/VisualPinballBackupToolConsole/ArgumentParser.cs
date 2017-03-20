abe71badb73380425cf336f3e1ffcf7407b0b19e
digable <quincy.fuller@gmail.com>
2017-03-20 00:40:24 -0500

    initial commit

---
+++ VisualPinballBackupToolConsole/VisualPinballBackupToolConsole/ArgumentParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualPinballBackupToolConsole
+{
+    public class ArgumentParser
+    {
+        public static Arguments Parse(string[] args)
+        {
+            Arguments a = new Arguments();
+
+            List<string> lArgs = args.ToList<string>();
+
+            if (args.Length == 0 || args.Contains("-h") || args.Contains("?"))
+            {
+                //INFO: write out the help file
+                Console.WriteLine("\t-backup [location to save the backup file]");
+                //Console.WriteLine("\t-restore [file path to backup file]");
+            }
+            else
+            {
+                //INFO: parse the actual arguments
+                if (args.Contains("-backup"))
+                {
+                    int backupValueIndex = lArgs.IndexOf("-backup") + 1;
+                    string backupValue = lArgs[backupValueIndex];
+                    if (!Directory.Exists(backupValue))
+                    {
+                        //INFO: directory doesnt exit. Try again.
+                        Console.WriteLine("The directory '" + backupValue + "' does not exist.");
+                    }
+                    else
+                    {
+                        a.Action = ActionTypes.BackupPinballX;
+                        a.Directory = backupValue;
+                    }
+                }
+            }
+
+            return a;
+        }
+    }
+
+    public class Arguments
+    {
+        public ActionTypes Action { get; set; }
+        public string Directory { get; set; }
+    }
+
+    public enum ActionTypes
+    {
+        BackupPinballX = 1
+    }
+}
---
+++ VisualPinballBackupToolConsole/VisualPinballBackupToolConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualPinballBackupToolConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Arguments backup = ArgumentParser.Parse(args);
+
+            switch(backup.Action)
+            {
+                case ActionTypes.BackupPinballX:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
